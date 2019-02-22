3da690d93010f5c388f3f1b2de7a9bf543bbd4c3
Katarzyna Moskała <kasia.moskala.20.03@gmail.com>
2019-02-23 00:24:58 +0100

    first

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+
+var gulp = require('gulp')
+  , minifyCss = require("gulp-minify-css")
+  , uglify = require("gulp-uglify")
+  , sass = require("gulp-sass");
+
+sass.compiler = require('node-sass');
+
+
+// task uglify js
+gulp.task('js', function () {
+ return gulp.src('js/*.js') // path to your files
+    .pipe(uglify())
+    .pipe(gulp.dest('js.min'));
+});
+// task sass to css
+gulp.task('sass', function () {
+  return gulp.src('scss/*.scss') // path to your file
+    .pipe(sass().on('error', sass.logError))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./scss/*.scss', ['sass']);
+  gulp.watch('./js/*.js', ['js']);
+});
