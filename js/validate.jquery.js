380b927e3850171307df3bac853c0ab77e46c8c3
Katarzyna Moskała <kasia.moskala.20.03@gmail.com>
2019-02-25 13:34:49 +0100

    plugin

--- js/validate.jquery.js
+++ js/validate.jquery.js
@@ -20,9 +20,9 @@
 			message    : '',
 			messageMinLeght : 'Text jest za krótki!',
 			messageMaxLenght : 'Text jest za długi!',
-			messageRequire : 'To pole jest wymagane',
-			messageIsPhone : 'To musi być nr telefonu w formacie: 444-444-444',
-			messageIsEmail  : 'To musi być e-mail w formacie: jakismail@cos.pl'
+			messageRequire : 'To pole jest wymagane.',
+			messageIsPhone : 'To musi być nr telefonu w formacie: 444-444-444.',
+			messageIsEmail  : 'To musi być e-mail w formacie: jakismail@cos.pl.'
 		}, config);
 		
 		return this.each(function() {
@@ -30,21 +30,22 @@
 			const $input = $t.find('.inputContainer');
 			const $submit = $t.find('.button');
 			
-			const minLenghtIn = $input.find("[data-minLenght]").data("minLenght");
-			const maxLenghtIn = $input.find("[data-maxLenght]").data("maxLenght");
-			const requireIn   = $input.find("[data-require]").data("require");
-			const isPhoneIn   = $input.find("[data-isPhone]").data("isPhone");
-			const isEmailIn   = $input.find("[data-isEmail]").data("isEmail");
+			
 			function check(input) {
 				input.each(function () {
-					
 					const $inputField = $(this).find('.input');
 					const $errorMessage = $(this).find('.error');
+					const minLenghtIn = $inputField.data("minlenght");
+					const maxLenghtIn = $inputField.data("maxlenght");
+					const requireIn   = $inputField.data("require");
+					const isPhoneIn   = $inputField.data("isphone");
+					const isEmailIn   = $inputField.data("isemail");
+					var $message = '';
 					//min lenght Input
 					if(minLenghtIn !== options.minLength) {
-						if($inputField.val().length < minLenghtIn) {
+						if($inputField.val().length< minLenghtIn) {
 							$(this).removeClass("valid").addClass("invalid");
-							$errorMessage.text(options.messageMinLeght + '\n');
+							$message += options.messageMinLeght + '\n';
 							
 						} else {
 							$(this).removeClass("invalid").addClass("valid");
@@ -54,7 +55,7 @@
 					} else {
 						if($inputField.val().length < options.minLength) {
 							$(this).removeClass("valid").addClass("invalid");
-							$errorMessage.text(options.messageMinLeght + '\n');
+							$message += options.messageMinLeght + '\n';
 							
 						} else {
 							$(this).removeClass("invalid").addClass("valid");
@@ -68,7 +69,8 @@
 					if(maxLenghtIn !== options.maxLength) {
 						if($inputField.val().length > minLenghtIn) {
 							$(this).removeClass("valid").addClass("invalid");
-							$errorMessage.text(options.messageMaxLenght + '\n');
+							$message += options.messageMaxLenght + '\n';
+							
 						} else {
 							$input.removeClass("invalid").addClass("valid");
 							$errorMessage.text('');
@@ -77,7 +79,8 @@
 					} else {
 						if($inputField.val().length > options.maxLength) {
 							$(this).removeClass("valid").addClass("invalid");
-							$errorMessage.text(options.messageMaxLenght + '\n');
+							$message += options.messageMaxLenght + '\n';
+							
 						} else {
 							$(this).removeClass("invalid").addClass("valid");
 							$errorMessage.text('');
@@ -88,7 +91,7 @@
 					if(requireIn) {
 						if($inputField.val().length === 0) {
 							$(this).removeClass("valid").addClass("invalid");
-							$errorMessage.text(options.messageRequire + '\n');
+							$message += options.messageRequire + '\n';
 						} else {
 							$(this).removeClass("invalid").addClass("valid");
 							$errorMessage.text('');
@@ -100,7 +103,8 @@
 						const regexPhonePat = options.phoneRegex.test($inputField.val());
 						if(!regexPhonePat) {
 							$(this).removeClass("valid").addClass("invalid");
-							$errorMessage.text(options.messageRequire + '\n');
+							$message += options.messageIsPhone + '\n';
+							
 						} else {
 							$(this).removeClass("invalid").addClass("valid");
 							$errorMessage.text('');
@@ -113,35 +117,39 @@
 						const regexPat = options.emailRegex.test($inputField.val());
 						if(!regexPat) {
 							$(this).removeClass("valid").addClass("invalid");
-							$errorMessage.text(options.messageRequire + '\n');
+							$message += options.messageIsEmail + '\n';
+							
 						} else {
 							$(this).removeClass("invalid").addClass("valid");
 							$errorMessage.text('');
 							
 						}
 					}
-					if($(this).hasClass("valid") && !$(this).hasClass("invalid") ) {
-						$(this).css({
-							"color" : "#2EF285",
-						});
-						$inputField.css({
-							"border" : "1px solid #2EF285"
-						})
-					} else if(!$(this).hasClass("valid") && $(this).hasClass("invalid")  ) {
-						$(this).css({
-							"color" : "#EB0801",
-						});
-						$inputField.css({
-							"border" : "1px solid #EB0801"
-						})
-					}
+					
+					
+					$errorMessage.text($message);
 				});
 				
 			}
 			$input.each(function () {
-				$(this).on('blur', function () {
-					check($(this));
-				})
+				$(this).find('.input').blur( function () {
+					check($input);
+				});
+				if($(this).hasClass("valid") && !$(this).hasClass("invalid") ) {
+					$(this).css({
+						"color" : "#2EF285",
+					});
+					$inputField.css({
+						"border" : "1px solid #2EF285"
+					})
+				} else if(!$(this).hasClass("valid") && $(this).hasClass("invalid")  ) {
+					$(this).css({
+						"color" : "#EB0801",
+					});
+					$inputField.css({
+						"border" : "1px solid #EB0801"
+					})
+				}
 			});
 			
 			$submit.bind('click', function (e) {
